56fd571e9347744f553519530eb2effe391e6888
Elbert Cortez <elbertraudry@gmail.com>
2019-01-18 14:37:27 -0500

    add homepage

---
+++ src/content/home.js
@@ -0,0 +1,27 @@
+import openSourceIcon from '../images/open-source-icon.svg';
+import protectionIcon from '../images/protection-icon.svg';
+import usabilityIcon from '../images/usability-icon.svg';
+
+export default {
+    head: 'Public URLs for exposing your local web server.',
+    subhead: 'Spend more time programming. One command for an instant, secure URL to your localhost server through any NAT or firewall.',
+    why: 'Dignissimos, beatae earum? Quibusdam, nisi earum distinctio numquam, minima architecto deleniti obcaecati dicta libero dolorum maxime, nobis adipisci ratione amet in nihil consequatur mollitia. Ipsam, nihil! Nihil magnam voluptatibus nostrum dicta vitae quaerat?',
+    howItWorks: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat fermentum mauris, tristique dignissim nisi viverra nec adipiscing elit consequat. <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat fermentum mauris, tristique dignissim nisi viverra nec.",
+    features: [
+        {
+            title: 'FOSS',
+            description: 'Open source both on the client and the server.',
+            icon: openSourceIcon
+        },
+        {
+            title: 'Secure',
+            description: 'Multiple architectural and implementation details that guard your use of our service.',
+            icon: protectionIcon
+        },
+        {
+            title: 'Easy',
+            description: 'The entire service is intuitive to use and does what it is supposed to do.',
+            icon: usabilityIcon
+        }
+    ]
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,59 @@
 import React from 'react'
+import Img from 'gatsby-image'
+import { Link, graphql } from 'gatsby'
 
-import Layout from '../components/layout'
+import '../styles/home.module.scss'
+
+import content from '../content/home'
 import SEO from '../components/seo'
 import Hero from '../components/Hero'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={['holepunch', 'application', 'tunneling']} />
-    <Hero>
-      <h1>Hi</h1>
-      <p>
-        Holepunch site.
-      </p>
-    </Hero>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  return (
+    <>
+      <SEO title="Home" keywords={['holepunch', 'application', 'tunneling']} />
+      <Hero head={content.head} subhead={content.subhead} />
+      <div styleName="why" className="container page__header text-center">
+        <h2>Why holepunch?</h2>
+        <p>{content.why}</p>
+      </div>
+      <div styleName="how" className="container">
+        <div styleName="how__body">
+          <h2>How it works?</h2>
+          <p dangerouslySetInnerHTML={{ __html: content.howItWorks }} />
+        </div>
+        <div styleName="how__media">
+          <Img fluid={data.how.childImageSharp.fluid} />
+        </div>
+      </div>
+      <div styleName="features" className="container">
+        <ul>
+          {content.features.map((feature, i) => (
+            <li key={i}>
+              <img src={feature.icon} alt={feature.title} />
+              <div>
+                <h6>{feature.title}</h6>
+                <p>{feature.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <Link to="/features">Check out more features.</Link>
+      </div>
+    </>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    how: file(relativePath: { eq: "holepunch-how.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
