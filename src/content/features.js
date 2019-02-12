0a66e69966b8462d36232084d89dbe9e322a0f98
Corbin Champion <corbinlc@gmail.com>
2019-02-12 14:00:44 -0800

    update the features page enough for beta release

--- src/content/features.js
+++ src/content/features.js
@@ -5,42 +5,8 @@ import usabilityIcon from '../images/usability-icon.svg';
 export default {
   pageTitle: 'Features',
   pageSubtitle:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae quis reiciendis',
+    'Everything you need to share your local servers with the world.',
   features: [
-    {
-      title: 'FOSS',
-      description: 'Open source both on the client and the server.',
-      icon: openSourceIcon,
-    },
-    {
-      title: 'Secure',
-      description:
-        'Multiple architectural and implementation details that guard your use of our service.',
-      icon: protectionIcon,
-    },
-    {
-      title: 'Easy',
-      description:
-        'The entire service is intuitive to use and does what it is supposed to do.',
-      icon: usabilityIcon,
-    },
-    {
-      title: 'FOSS',
-      description: 'Open source both on the client and the server.',
-      icon: openSourceIcon,
-    },
-    {
-      title: 'Secure',
-      description:
-        'Multiple architectural and implementation details that guard your use of our service.',
-      icon: protectionIcon,
-    },
-    {
-      title: 'Easy',
-      description:
-        'The entire service is intuitive to use and does what it is supposed to do.',
-      icon: usabilityIcon,
-    },
     {
       title: 'FOSS',
       description: 'Open source both on the client and the server.',
