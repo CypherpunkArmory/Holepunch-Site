b35954594f5975e7443393988a8be8eff4f72d61
Corbin Champion <corbinlc@gmail.com>
2019-02-14 09:59:42 -0800

    change catch phrase

--- src/content/home.js
+++ src/content/home.js
@@ -3,8 +3,8 @@ import protectionIcon from '../images/protection-icon.svg';
 import usabilityIcon from '../images/usability-icon.svg';
 
 export default {
-    head: 'Public URLs/IPs for exposing local servers.',
-    subhead: "Expose local servers running behind any NAT or firewall.  This could be a web server, an FTP server, or even a game server.  You start it and we take care of the rest.",
+    head: 'No cloud, no problem.',
+    subhead: "Run whatever you want at home or the office.  This could be a web server, an FTP server, or even a game server.  You start it and we take care of the rest.  We will get you past any NAT or firewall that might be in the way and people will be able to access your service via a public URL.",
     why: "Want security and privacy, then host the server yourself.  Want low cost, then host the server yourself.  Want maximum flexibility, then host the server yourself.  With our low cost, secure tunneling service, you won't run out or reasons or ways to use it.",
     howItWorks: "After signing up for our service, you simply download and use our intutative CLI to create a tunnel.  This makes a connection between your local server and our servers.  Our servers then expose your service at a public URL/IP.  You can choose a subdomain or have a random one assigned.",
     features: [
