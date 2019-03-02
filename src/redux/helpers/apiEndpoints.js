d52ce71863ebbcc1ef037ee8ff018838c22f0d27
Corbin Champion <corbinlc@gmail.com>
2019-03-01 16:29:40 -0800

    https-everywhere

--- src/redux/helpers/apiEndpoints.js
+++ src/redux/helpers/apiEndpoints.js
@@ -1,5 +1,5 @@
 const origin =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'http://api.holepunch.io/'
+  process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'https://api.holepunch.io/'
 
 export default {
   // Accounts
