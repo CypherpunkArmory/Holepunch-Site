788b39dda35216ebb512271ced81795212a91e93
Elbert Cortez <elbertraudry@gmail.com>
2019-05-16 16:13:52 -0500

    Add faq page to main nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     menu: [
       { name: 'Features', route: '/features' },
       { name: 'Download', route: '/download' },
+      { name: 'FAQ', route: '/faq' },
       { name: 'Docs', route: '/docs' },
       { name: 'Login', route: '/login' },
       { name: 'Sign up', route: '/signup' },
