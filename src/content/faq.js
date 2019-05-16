b1ee0229c7c4fa74c723403e99f6559801a26245
Elbert Cortez <elbertraudry@gmail.com>
2019-05-16 15:41:21 -0500

    add FAQ page

---
+++ src/content/faq.js
@@ -0,0 +1,36 @@
+export default {
+  pageTitle: 'FAQ',
+  pageSubtitle: 'Omnis mollitia veritatis quia vel nulla possimus',
+  questions: [
+    {
+      name: 'Culpa necessitatibus animi aliquam?',
+      value:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis mollitia veritatis quia vel nulla possimus odio fugiat eum assumenda, illum quod inventore dolore! Quod iusto obcaecati nesciunt repudiandae odio tempora?',
+    },
+    {
+      name: 'Inventore eos  assumenda expedita blanditiis, maxime?',
+      value:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis mollitia veritatis quia vel nulla possimus odio fugiat eum assumenda, illum quod inventore dolore! Quod iusto obcaecati nesciunt repudiandae odio tempora?',
+    },
+    {
+      name: 'Consectetur adipisicing elit?',
+      value:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis mollitia veritatis quia vel nulla possimus odio fugiat eum assumenda, illum quod inventore dolore! Quod iusto obcaecati nesciunt repudiandae odio tempora?',
+    },
+    {
+      name: 'Culpa necessitatibus animi aliquam?',
+      value:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis mollitia veritatis quia vel nulla possimus odio fugiat eum assumenda, illum quod inventore dolore! Quod iusto obcaecati nesciunt repudiandae odio tempora?',
+    },
+    {
+      name: 'Assumenda expedita blanditiis, maxime?',
+      value:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis mollitia veritatis quia vel nulla possimus odio fugiat eum assumenda, illum quod inventore dolore! Quod iusto obcaecati nesciunt repudiandae odio tempora?',
+    },
+    {
+      name: 'Consectetur adipisicing elit?',
+      value:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis mollitia veritatis quia vel nulla possimus odio fugiat eum assumenda, illum quod inventore dolore! Quod iusto obcaecati nesciunt repudiandae odio tempora?',
+    },
+  ],
+}
